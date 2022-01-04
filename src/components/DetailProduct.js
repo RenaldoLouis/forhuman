b86e0bc1262184c75691418370e87fb214d06f6e
RenaldoLouis <renaldolouis555@gmail.com>
2022-01-04 18:06:51 +0700

    try to fix slider detail infinite false empty space on the right

--- src/components/DetailProduct.js
+++ src/components/DetailProduct.js
@@ -86,9 +86,9 @@ function DetailProduct() {
     const settings = {
         dots: false,
         infinite: true,
-        speed: 500,
-        slidesToShow: isMobile ? 1 : 1,
-        slidesToScroll: 1
+        speed: 1000,
+        slidesToShow: isMobile ? 1 : 3,
+        slidesToScroll: 3
     };
 
     useEffect(() => {
@@ -101,9 +101,9 @@ function DetailProduct() {
     return (
         <Grid container spacing={2}>
 
-            <Grid id="slider" sx={{ pt: 2 }} xs={12}>
+            <Grid style={{ overflow: "hidden" }} sx={{ pt: 2 }} xs={12}>
                 {/* <Container sx={{ pl: 5 }}> */}
-                <Slider sx={{ height: '100%', }} style={{ width: "100vw" }} id="slider" {...settings}>
+                <Slider style={{ overflow: "hidden" }} sx={{ height: '100%', width: "100%", overflow: "hidden" }} id="slider" {...settings}>
                     <img key="image1" sx={{ height: '100%', }} className="fullImage" src={selectedDetailData.detailImage1} alt={selectedDetailData.name}></img>
                     <img key="image2" sx={{ height: '100%', }} className="fullImage" src={selectedDetailData.detailImage2} alt={selectedDetailData.name}></img>
                     <img key="image3" sx={{ height: '100%', }} className="fullImage" src={selectedDetailData.detailImage2} alt={selectedDetailData.name}></img>
